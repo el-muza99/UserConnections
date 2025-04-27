e3ac16e0fa255c087548b17a936fe9851bf96436
madiyar <madiyar@>
2025-04-28 04:37:42 +0500

    added auto database scheme update by migrations

--- UserConnections.Api/Program.cs
+++ UserConnections.Api/Program.cs
@@ -1,6 +1,8 @@
 using System.Reflection;
 using UserConnections.Application;
 using UserConnections.Infrastructure;
+using UserConnections.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +18,13 @@ builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
+
+// Apply migrations automatically at startup
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<UserConnectionDbContext>();
+    dbContext.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
